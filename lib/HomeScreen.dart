f995cdf60283fb8f0a2bb7b0e96adc6263ac754f
Rajaram <95975033+rajarampatra73@users.noreply.github.com>
2023-11-17 20:55:46 +0530

    home screen

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/todo_Bloc/todo_bloc.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,8 +17,27 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Theme.of(context).colorScheme.background,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        onPressed: (){},
-        child: Icon(CupertinoIcons.add, color: Colors.black,),
+        onPressed: () {},
+        child: Icon(
+          CupertinoIcons.add,
+          color: Colors.black,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: BlocBuilder<TodoBloc, TodoState>(
+          builder: (context, state) {
+            if (state.status == TodoStatus.success) {
+              return Container();
+            } else if (state.status == TodoStatus.initial) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
       ),
     );
   }
